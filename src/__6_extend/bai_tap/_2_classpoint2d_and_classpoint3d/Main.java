bb4573c5ce959ccc9322f7d8dac606fcca938ebc
cuongyura <c95.ficf@gmail.com>
2020-09-15 10:44:26 +0700

    update

---
+++ src/__6_extend/bai_tap/_2_classpoint2d_and_classpoint3d/Main.java
@@ -0,0 +1,19 @@
+package __6_extend.bai_tap._2_classpoint2d_and_classpoint3d;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Please input x: ");
+        float x = sc.nextFloat();
+        System.out.println("Please input x: ");
+        float y = sc.nextFloat();
+        System.out.println("Please input x: ");
+        float z = sc.nextFloat();
+        Point2D point2D = new Point2D(x,y);
+        System.out.println(point2D.toString());
+        Point3D point3D = new Point3D(x,y,z);
+        System.out.println(point3D.toString());
+    }
+}
